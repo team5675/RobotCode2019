dc0bd229741594ecc4b04c6af0a4dd92c65bbfc6
minemax03 <minemax03@gmail.com>
2019-01-26 16:17:06 -0500

    More DriverController

--- src/main/java/frc/robot/DriverController.java
+++ src/main/java/frc/robot/DriverController.java
@@ -18,7 +18,7 @@ public class DriverController {
     XboxController xbox2 = new XboxController(1);//aux driver
 
     public double getForward() {
-        return -xbox1.getRawAxis(0);
+        return -(xbox1.getRawAxis(0));
     }
 
     public double getStrafe() {
@@ -47,5 +47,29 @@ public class DriverController {
 
     public boolean getClimb3() {
         return xbox1.getBumper(Hand.kRight);
+    }
+    public boolean getHatchIntake() {
+        return xbox2.getBumper(Hand.kLeft);
+    }
+    public boolean getHatchRelease() {
+        return (xbox2.getTriggerAxis(Hand.kLeft)) == 1; 
+    }
+    public boolean getBumperIntake() {
+        return xbox2.getBumper(Hand.kRight);
+    }
+    public boolean getBumperRelease() {
+        return (xbox2.getTriggerAxis(Hand.kRight)) == 1;
+    }
+    public double getElevator() {
+        return -(xbox2.getRawAxis(0));
+    }
+    public boolean getCargo1() {
+        return xbox2.getAButton();
+    }
+    public boolean getCargo2() {
+        return xbox2.getBButton();
+    }
+    public boolean getCargo3() {
+        return xbox2.getYButton();
     }
 }
