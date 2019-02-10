cbf0a11fc32a943c5b37d3884c04793927cc275f
Sargeantfish <sargeantfishcoc@gmail.com>
2019-02-10 15:36:45 -0500

    Ham might work now

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -14,6 +14,7 @@ import frc.robot.auto.LineUp;
 import frc.robot.subsystems.Jeff;
 import frc.robot.subsystems.Light;
 import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Ham;
 
 public class Robot extends TimedRobot {
 
@@ -23,6 +24,7 @@ public class Robot extends TimedRobot {
   Jeff jeff = new Jeff();
   Light light = new Light();
   Elevator elevator = new Elevator();
+  Ham ham = new Ham();
 
   @Override
   public void robotInit() {
@@ -65,6 +67,9 @@ public class Robot extends TimedRobot {
     }
     //elevator controls
     elevator.run();
+
+    //ham controls
+    ham.startClimbing();
   }
 
   
